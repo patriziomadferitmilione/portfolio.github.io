cb223c2db726de8792bb71ca9030bbebff608d40
Patrizio Milione <patriziomilione@gmail.com>
2023-05-19 10:55:44 +0200

    gallery - start

--- script.js
+++ script.js
@@ -1,3 +1,189 @@
+
+// let daje = ["https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/1.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/2.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/3.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/4.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/5.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/6.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/7.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/8.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/9.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/10.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/11.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/12.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/13.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/14.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/15.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/16.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/17.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/18.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/19.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/20.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/21.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/22.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/23.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/24.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/25.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/26.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/27.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/28.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/29.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/30.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/31.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/32.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/33.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/34.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/35.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/36.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/37.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/38.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/39.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/40.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/41.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/42.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/43.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/44.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/45.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/46.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/47.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/48.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/49.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/50.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/51.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/52.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/53.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/54.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/55.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/56.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/57.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/58.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/59.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/60.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/61.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/62.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/63.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/64.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/65.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/66.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/67.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/68.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/69.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/70.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/71.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/72.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/73.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/74.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/75.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/76.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/77.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/78.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/79.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/80.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/81.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/82.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/83.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/84.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/85.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/86.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/87.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/88.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/89.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/90.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/91.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/92.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/93.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/94.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/95.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/96.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/97.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/98.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/99.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/100.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/101.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/102.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/103.jpg",
+// "https://raw.githubusercontent.com/patriziomadferitmilione/media/main/portfolio/Music/104.jpg"]
+
+// let string
+
+// for (i=0; i<105; i++) {
+//   string += `<figure
+//   itemprop="associatedMedia"
+//   itemscope
+//   itemtype="http://schema.org/ImageObject"
+// >
+//   <a
+//     href="${daje[i]}"
+//     itemprop="contentUrl"
+//     data-size="1024x1024"
+//   >
+//     <img
+//       src="${daje[i]}"
+//       itemprop="thumbnail"
+//       alt="Image description"
+//     />
+//   </a>
+//   <figcaption itemprop="caption description">
+//     Image caption 1
+//   </figcaption>
+// </figure> `
+// }
+
+
+
+// console.log(string)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // SCROLL ANIMATIONS
 AOS.init();
 
@@ -51,3 +237,210 @@ balls.forEach((el, i, ra) => {
     }
   );
 });
+
+
+// GALLERY
+
+var initPhotoSwipeFromDOM = function (gallerySelector) {
+  // parse slide data (url, title, size ...) from DOM elements
+  // (children of gallerySelector)
+  var parseThumbnailElements = function (el) {
+    var thumbElements = el.childNodes,
+      numNodes = thumbElements.length,
+      items = [],
+      figureEl,
+      linkEl,
+      size,
+      item;
+
+    for (var i = 0; i < numNodes; i++) {
+      figureEl = thumbElements[i]; // <figure> element
+
+      // include only element nodes
+      if (figureEl.nodeType !== 1) {
+        continue;
+      }
+
+      linkEl = figureEl.children[0]; // <a> element
+
+      size = linkEl.getAttribute("data-size").split("x");
+
+      // create slide object
+      item = {
+        src: linkEl.getAttribute("href"),
+        w: parseInt(size[0], 10),
+        h: parseInt(size[1], 10),
+      };
+
+      if (figureEl.children.length > 1) {
+        // <figcaption> content
+        item.title = figureEl.children[1].innerHTML;
+      }
+
+      if (linkEl.children.length > 0) {
+        // <img> thumbnail element, retrieving thumbnail url
+        item.msrc = linkEl.children[0].getAttribute("src");
+      }
+
+      item.el = figureEl; // save link to element for getThumbBoundsFn
+      items.push(item);
+    }
+
+    return items;
+  };
+
+  // find nearest parent element
+  var closest = function closest(el, fn) {
+    return el && (fn(el) ? el : closest(el.parentNode, fn));
+  };
+
+  // triggers when user clicks on thumbnail
+  var onThumbnailsClick = function (e) {
+    e = e || window.event;
+    e.preventDefault ? e.preventDefault() : (e.returnValue = false);
+
+    var eTarget = e.target || e.srcElement;
+
+    // find root element of slide
+    var clickedListItem = closest(eTarget, function (el) {
+      return el.tagName && el.tagName.toUpperCase() === "FIGURE";
+    });
+
+    if (!clickedListItem) {
+      return;
+    }
+
+    // find index of clicked item by looping through all child nodes
+    // alternatively, you may define index via data- attribute
+    var clickedGallery = clickedListItem.parentNode,
+      childNodes = clickedListItem.parentNode.childNodes,
+      numChildNodes = childNodes.length,
+      nodeIndex = 0,
+      index;
+
+    for (var i = 0; i < numChildNodes; i++) {
+      if (childNodes[i].nodeType !== 1) {
+        continue;
+      }
+
+      if (childNodes[i] === clickedListItem) {
+        index = nodeIndex;
+        break;
+      }
+      nodeIndex++;
+    }
+
+    if (index >= 0) {
+      // open PhotoSwipe if valid index found
+      openPhotoSwipe(index, clickedGallery);
+    }
+    return false;
+  };
+
+  // parse picture index and gallery index from URL (#&pid=1&gid=2)
+  var photoswipeParseHash = function () {
+    var hash = window.location.hash.substring(1),
+      params = {};
+
+    if (hash.length < 5) {
+      return params;
+    }
+
+    var vars = hash.split("&");
+    for (var i = 0; i < vars.length; i++) {
+      if (!vars[i]) {
+        continue;
+      }
+      var pair = vars[i].split("=");
+      if (pair.length < 2) {
+        continue;
+      }
+      params[pair[0]] = pair[1];
+    }
+
+    if (params.gid) {
+      params.gid = parseInt(params.gid, 10);
+    }
+
+    return params;
+  };
+
+  var openPhotoSwipe = function (
+    index,
+    galleryElement,
+    disableAnimation,
+    fromURL
+  ) {
+    var pswpElement = document.querySelectorAll(".pswp")[0],
+      gallery,
+      options,
+      items;
+
+    items = parseThumbnailElements(galleryElement);
+
+    // define options (if needed)
+    options = {
+      // define gallery index (for URL)
+      galleryUID: galleryElement.getAttribute("data-pswp-uid"),
+
+      getThumbBoundsFn: function (index) {
+        // See Options -> getThumbBoundsFn section of documentation for more info
+        var thumbnail = items[index].el.getElementsByTagName("img")[0], // find thumbnail
+          pageYScroll =
+            window.pageYOffset || document.documentElement.scrollTop,
+          rect = thumbnail.getBoundingClientRect();
+
+        return { x: rect.left, y: rect.top + pageYScroll, w: rect.width };
+      },
+    };
+
+    // PhotoSwipe opened from URL
+    if (fromURL) {
+      if (options.galleryPIDs) {
+        // parse real index when custom PIDs are used
+        // http://photoswipe.com/documentation/faq.html#custom-pid-in-url
+        for (var j = 0; j < items.length; j++) {
+          if (items[j].pid == index) {
+            options.index = j;
+            break;
+          }
+        }
+      } else {
+        // in URL indexes start from 1
+        options.index = parseInt(index, 10) - 1;
+      }
+    } else {
+      options.index = parseInt(index, 10);
+    }
+
+    // exit if index not found
+    if (isNaN(options.index)) {
+      return;
+    }
+
+    if (disableAnimation) {
+      options.showAnimationDuration = 0;
+    }
+
+    // Pass data to PhotoSwipe and initialize it
+    gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
+    gallery.init();
+  };
+
+  // loop through all gallery elements and bind events
+  var galleryElements = document.querySelectorAll(gallerySelector);
+
+  for (var i = 0, l = galleryElements.length; i < l; i++) {
+    galleryElements[i].setAttribute("data-pswp-uid", i + 1);
+    galleryElements[i].onclick = onThumbnailsClick;
+  }
+
+  // Parse URL and open gallery if it contains #&pid=3&gid=1
+  var hashData = photoswipeParseHash();
+  if (hashData.pid && hashData.gid) {
+    openPhotoSwipe(hashData.pid, galleryElements[hashData.gid - 1], true, true);
+  }
+};
+
+// execute above function
+initPhotoSwipeFromDOM(".my-gallery");
